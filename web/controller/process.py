466d1f0bfe0f7eb504fd1b616c05cc22089ffb50
ccdroid <ccdroid@163.com>
2024-08-19 07:40:00 +0800

    adust delay,prove stabilty

--- web/controller/process.py
+++ web/controller/process.py
@@ -75,7 +75,7 @@ class WebServer:
                 await self.lock.acquire()  # 获取锁
                 try:
                     self.connection_id, res = self.network.esp_rcvData()
-                    await asyncio.sleep(0.1)
+                    await asyncio.sleep(0.2)
                 finally:
                     self.lock.release()  # 释放锁
 
@@ -103,10 +103,9 @@ class WebServer:
                         content_length = len(webpage_content)
                         await self.lock.acquire()  # 获取锁
                         try:
-                            # 准备网页内容
-                            await asyncio.sleep(0.1)  # 确保发送过程平稳，避免设备负载过高
                             # 发送 HTTP 响应头
                             self.network.esp_sendData(self.connection_id, f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {content_length}\r\n\r\n')
+                            await asyncio.sleep(0.2)  # 确保发送过程平稳，避免设备负载过高
                         finally:
                             self.lock.release()  # 释放锁
 
@@ -114,13 +113,13 @@ class WebServer:
                         for chunk in self.webpage():
                             await self.lock.acquire()  # 获取锁
                             try:
-                                await asyncio.sleep(0.1)  # 确保发送过程平稳，避免设备负载过高
                                 self.network.esp_sendData(self.connection_id, chunk)
+                                await asyncio.sleep(0.2)  # 确保发送过程平稳，避免设备负载过高
                             finally:
                                 self.lock.release()  # 释放锁
 
                         self.network.esp_sendData(self.connection_id, '\r\n')  # 发送结束标志
-                        await asyncio.sleep(0.1)  # 添加适当的延迟以避免过载
+                        await asyncio.sleep(0.2)  # 添加适当的延迟以避免过载
 
 
             except OSError as e:
